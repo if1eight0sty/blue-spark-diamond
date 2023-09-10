04341e1cebc9b009075a81dd085b3489438f771f
A18yan <ma18yan@gmail.com>
2023-09-10 21:06:55 +0545

    scroll

--- app/components/_scroll/scroll.tsx
+++ app/components/_scroll/scroll.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import React from 'react'
 import AOS from "aos";
 import 'aos/dist/aos.css';
--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <main className="bg-white dark:bg-white">
       <TopHero />
-      {/*   <CraftingBeauty />
-      <DiamondCut />*/}
+      <CraftingBeauty />
+      <DiamondCut />
       <FourCs />
     </main>
   )
