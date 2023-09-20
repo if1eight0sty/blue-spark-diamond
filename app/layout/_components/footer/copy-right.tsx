6f88e6e015329d0321a87c0483f28695f317f8b3
bikaldhimal <bikaldhimal2016@gmail.com>
2023-09-20 21:00:08 +0545

    map image added

--- app/layout/_components/footer/copy-right.tsx
+++ app/layout/_components/footer/copy-right.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const CopyRight = () => {
-    return (
-        <>
-            <div className="flex flex-col w-full items-center lg:mt-10">
-                <div className="py-5 flex flex-col md:flex-row items-center justify-between gap-2">
-                    <p className="text-sm font-light text-[13px] max-w-xs text-center text-[#585858]">
-                        &copy;2023 Blue Spark Diamond | All Rights Reserved.
-                    </p>
-                    <p className="text-sm font-light text-[13px] max-w-xs text-center text-[#23527c] hover:text-[#3d5b75] cursor-pointer">
-                        Privacy Policy
-                    </p>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex flex-col w-full items-center lg:mt-10">
+        <div className="py-5 flex flex-col md:flex-row items-center justify-between gap-2">
+          <p className="text-sm font-light text-[13px] max-w-xs text-center text-[#585858]">
+            &copy;2023 Blue Spark Diamond | All Rights Reserved.
+          </p>
+          <p className="text-sm font-light text-[13px] max-w-xs text-center text-[#23527c] hover:text-[#3d5b75] cursor-pointer">
+            Privacy Policy
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default CopyRight
+export default CopyRight;
