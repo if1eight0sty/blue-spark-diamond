61875675e80048c2bc203a4ca72a2fec643589eb
bikaldhimal <bikaldhimal2016@gmail.com>
2023-09-25 20:30:31 +0545

    diamond-cut updated

--- app/components/diamond-cut/diamond-cut.tsx
+++ app/components/diamond-cut/diamond-cut.tsx
@@ -59,67 +59,21 @@ const DiamondCut = () => {
 
   return (
     <>
-      <section className="flex flex-col items-center justify-center overflow-hidden bg-gray-800 py-16 font-poppins dark:bg-gray-800 isolate min-h-[95dvh]">
-        <div className="text-center my-10">
-          <span className="block mb-4 text-xs font-semibold leading-4 tracking-widest text-center text-blue-500 uppercase dark:text-gray-400">
-            Diamond
-          </span>
-          <h1 className="text-3xl font-bold capitalize text-white">
-            Cut and Shape
-          </h1>
+      <section className="flex flex-col items-center justify-center overflow-hidden bg-white font-poppins dark:bg-gray-400 isolate min-h-[95dvh]">
+        <div className="max-w-xl mx-auto text-gray-900 dark:text-white">
+          <h1 className="mb-4 text-3xl font-bold text-center">Cut and Shape</h1>
+          <p className="mb-16 text-base text-center mx-10">
+            Diamonds from Blue Spark are pure from every angle, from its cutting
+            to its clarity
+          </p>
         </div>
-        {/* <Swiper
-          effect={"coverflow"}
-          loop={true}
-          loopedSlides={4}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={4}
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2,
-            slideShadows: true,
-          }}
-          autoplay={{
-            delay: 1000,
-            disableOnInteraction: false,
-          }}
-          pagination={true}
-          modules={[Autoplay, EffectCoverflow, Pagination]}
-          className="mySwiper"
-        >
+        {/* image section */}
+        <div className="grid grid-cols-3 gap-10 md:gap-14 lg:gap-10 sm:grid-cols-3 lg:grid-cols-9 mt-10 bg-[#F7F6F2] p-10 md:p-20 lg:p-10  rounded-xl drop-shadow-md">
           {smallImages.map((image) => (
-            <SwiperSlide key={image.id}>
+            <div key={image.id} className="flex-shrink-0">
               <Link
                 href="/product"
-                className="flex flex-col gap-2 justify-center items-center bg-gray-800 h-full scale-95"
-              >
-                <Image
-                  width="200"
-                  height="200"
-                  src={image.src}
-                  style={{ width: "auto" }}
-                  title={`${image.id}-cut-diamond`}
-                  alt={`${image.id}`}
-                  loading="lazy"
-                  property=""
-                  className="h-[10em] object-fill"
-                />
-                <p className="text-white text-center text-lg font-medium capitalize">
-                  {image.id}
-                </p>
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper> */}
-        <div className="flex flex-col gap-5">
-          {smallImages.map((image) => (
-            <div key={image.id}>
-              <Link
-                href="/product"
-                className="flex flex-col gap-2 justify-center items-center bg-gray-800 h-full scale-95"
+                className="flex flex-col gap-2 justify-center items-center h-full scale-95"
               >
                 <Image
                   width="200"
@@ -132,11 +86,10 @@ const DiamondCut = () => {
                   property=""
                   className="h-[4em] object-fill"
                 />
-                <p className="text-white text-center text-lg font-medium capitalize">
+                <p className="text-black/60 text-center text-sm font-medium lowercase">
                   {image.id}
                 </p>
               </Link>
-              
             </div>
           ))}
         </div>
