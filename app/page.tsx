972910b57952da007ad2b40d9bab38a251be7d78
bikaldhimal <bikaldhimal2016@gmail.com>
2023-10-02 21:06:38 +0545

    global network added in home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import dynamic from "next/dynamic";
 
 const TopHero = dynamic(() => import("./components/top-hero/top-hero"));
 const CraftingBeauty = dynamic(() => import("./components/crafting-beauty"));
+
 const DiamondCut = dynamic(
   () => import("./components/diamond-cut/diamond-cut")
 );
-
+const GlobalNetwork = dynamic(
+  () => import("./components/global-network/global-network")
+);
 import FourCs from "./components/four-cs/four-cs";
 
 export default function Home() {
@@ -17,6 +20,7 @@ export default function Home() {
       <CraftingBeauty />
       <DiamondCut />
       <FourCs />
+      <GlobalNetwork />
     </main>
   );
 }
