18d09a0e6025bcbbb3006979883f1ea3a26c2fc2
A18yan <ma18yan@gmail.com>
2023-10-02 21:06:14 +0545

    team-member

--- app/company/_components/team/interface.ts
+++ app/company/_components/team/interface.ts
@@ -1,9 +1,8 @@
-import { StaticImageData } from "next/image";
 
 export interface ITeamInfo {
     name: string,
     designation: string,
     desc: string,
-    image: StaticImageData,
+    image: string,
     title: string
 }
--- app/company/_components/team/team.tsx
+++ app/company/_components/team/team.tsx
@@ -1,35 +1,53 @@
 import React from "react";
 import dynamic from "next/dynamic";
-
-import chairman from "@/public/team/chairman.webp";
-import ceo from "@/public/team/ceo.webp";
-import md from "@/public/team/md.webp";
+import { getCldImageUrl } from 'next-cloudinary';
 import { ITeamInfo } from "./interface";
 
 const TeamHeading = dynamic(() => import("./components/team-heading"));
 const TeamMember = dynamic(() => import("./components/team-member"));
 
 const Team = () => {
+  // get image url from cloudinary based on the image name/public id
+  const getImageURL = (
+    height: number,
+    width: number,
+    src: string,
+  ) => {
+    return getCldImageUrl({
+      width,
+      height,
+      src,
+    })
+  }
+
   const teamInfo: ITeamInfo[] = [
     {
       name: "Yadav Prasad Pathak",
       designation: "Chairman (The Man Behind Blue Spark Diamond)",
       desc: "Mr. Pathak, the visionary leader behind Blue Spark Diamond, has played a pivotal role since its inception. His guiding vision has been instrumental in shaping the company. Blue Diamond and Mr.Pathak both strive to restore elegance to diamonds.",
-      image: chairman,
+      image: getImageURL(
+        300,
+        600,
+        "blue-spark/team/chairman"
+      ),
       title: "Chairman: Yadav Prasad Pathak",
     },
     {
       name: "Sher Bahadur Karki",
       designation: "Chairman (The Man Behind Blue Spark Diamond)",
       desc: " With a keen eye for diamonds and extensive industry expertise, Mr.Karki is a seasoned figure in the Nepalese diamond market. With over 50 years of experience, he serves as the guiding force at Blue Spark, blending his experience and passion to create a strong presence.",
-      image: ceo,
+      image: getImageURL(300, 600, "blue-spark/team/ceo"),
       title: "CEO: Sher Bahadur Karki",
     },
     {
       name: "Rohit Bhattarai",
       designation: "Managing Director of the Blue Spark Diamond Company",
       desc: "As of July 13, 2021, Mr. Rohit Bhattarai serves as the acting managing director of Blue Spark Diamond International Company. With a decade of experience in the diamond industry, Mr. Bhattarai has notably worked within the Rough Diamond Division and various international agencies, primarily in the African Territory.His reputation for precision and strong leadership led to his appointment at Blue Spark in 2021.",
-      image: md,
+      image: getImageURL(
+        300,
+        600,
+        "blue-spark/team/md"
+      ),
       title: "Managing Director: Rohit Bhattarai",
     },
   ];
