b3cbff7e7162a3b733630edb4d16a01dac215dea
bikaldhimal <bikaldhimal2016@gmail.com>
2023-10-02 21:37:55 +0545

    image finalized for global network

--- app/components/global-network/global-network.tsx
+++ app/components/global-network/global-network.tsx
@@ -6,10 +6,13 @@ import globalnetwork from "@/public/global-network.webp";
 const GlobalNetwork = () => {
   return (
     <>
-      <section className="flex justify-between items-center bg-yellow-100 px-10 py-20">
+      <section className="flex justify-between gap-5 items-center bg-blue-100 px-10 py-20">
         <div className="flex flex-col max-w-xl">
-          <h3 className="text-7xl font-medium">Global Network</h3>
-          <p className="text-gray-500 font-normal">
+          <h6 className="uppercase text-sm font-base mb-2">
+            business across numerous countries
+          </h6>
+          <h3 className="text-6xl font-bold font-serif mb-5">Global Network</h3>
+          <p className="text-gray-500 font-normal text-justify">
             Blue Spark Diamond Company has vast experience working with
             different cultures, and conducting business across numerous
             countries around the globe. Within Blue Spark, we think globally and
@@ -19,14 +22,16 @@ const GlobalNetwork = () => {
             importing and exporting worldwide within 72 hours.
           </p>
         </div>
-        <Image
-          width={700}
-          height={500}
-          src={globalnetwork}
-          alt="global network"
-          priority
-          className=""
-        />
+        <div className="hidden md:block">
+          <Image
+            width={700}
+            height={500}
+            src={globalnetwork}
+            alt="global network"
+            priority
+            className=""
+          />
+        </div>
       </section>
     </>
   );
