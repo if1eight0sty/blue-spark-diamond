3a7586df24dc840538fc4df3bb52d4007228a91c
bikaldhimal <bikaldhimal2016@gmail.com>
2023-10-08 20:35:31 +0545

    images added for explore more section

--- app/company/_components/vision/_components/vision-heading.tsx
+++ app/company/_components/vision/_components/vision-heading.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const VisionHeading = () => {
-    return (
-        <>
-            <div className="max-w-xl text-gray-500">
-                <h1 className="mb-3 text-4xl font-bold">
-                    Our Vision
-                </h1>
-                <p className="mb-16 text-base ">
-                    We are moving forward with the objective of establishing our diamond company in the global market
-                </p>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="max-w-xl text-gray-500">
+        <h1 className="mb-3 text-4xl font-bold">Why Blue Spark</h1>
+        <p className="mb-16 text-base ">
+          We are moving forward with the objective of establishing our diamond
+          company in the global market
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default VisionHeading
+export default VisionHeading;
